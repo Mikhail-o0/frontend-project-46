d325b9def84fd7ab45ad932cd296440555fc5b6e
Mikhail Morozov <mixail.mo95@mail.ru>
2025-06-18 18:28:41 +0300

    made changes to gendiff.js

--- commands/gendiff.js
+++ commands/gendiff.js
@@ -4,6 +4,8 @@ const { program } = require('commander');
 
 program
   .name('gendiff')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
 
