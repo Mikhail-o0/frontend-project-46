62cef478453798b9279c0500a871316a8a714ab4
Mikhail Morozov <mixail.mo95@mail.ru>
2025-08-24 12:05:12 +0300

    update file

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,5 +1,5 @@
 const buildDiff = (obj1, obj2) => {
-  const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort()
+  const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort((a, b) => a.localeCompare(b))
 
   return keys.map((key) => {
     if (!Object.hasOwn(obj1, key)) {
